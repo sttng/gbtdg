083395ad1d8380456d7971f1cf53611173c49df3
sttng <46765778+sttng@users.noreply.github.com>
2021-05-10 15:44:51 +0800

    Update tiledata2img.js

--- tiledata2img.js
+++ tiledata2img.js
@@ -32,6 +32,7 @@ function refresh(canvas, rawBytes) {
 
 function decode(rawBytes) {
     var bytes = rawBytes.replace(/\$|,|0x| /g, "");
+    bytes = bytes.replace(/^db/, "");
     console.log(bytes.length % 32);
     if (bytes.length % 32 != 0) return false;
     
