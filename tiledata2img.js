307aa47f7f58fe69c9114d8487e746919d88a57a
sttng <46765778+sttng@users.noreply.github.com>
2021-05-12 13:14:00 +0800

    Update tiledata2img.js

--- tiledata2img.js
+++ tiledata2img.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-    width = 8;
-    height = 8;
+    width = 4;
+    height = 4;
     square_width = document.getElementById("demo_canvas").width / (width);
     square_height = document.getElementById("demo_canvas").height / (height);
 
